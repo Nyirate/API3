02f01a3fbdced0a8225363af0ed796cd40c7c4a1
Nyirate <rwakaemma34@gmail.com>
2019-10-27 12:17:01 +0200

    add new activity

---
+++ app/src/main/java/com/example/api1/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.api1.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.api1.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
